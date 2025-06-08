438fef7df84925be95bc5925535693f3a9c7536b
VeselinaKatsarova <24IMC10520@fh-krems.ac.at>
2025-06-08 20:19:07 +0200

    Update Task-5_B.py
    Add comments

--- Assigment3-DB/Task-5_B.py
+++ Assigment3-DB/Task-5_B.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+# connect to the database
 CONNECTION = "dbname=ski_db user=postgres password=secret host=localhost port=5432"
 
 conn = psycopg2.connect(CONNECTION)
@@ -8,8 +9,9 @@ cursor = conn.cursor()
 query = """
     SELECT show_chunks('skipass_telemetry');
     """
-cursor.execute(query)
+cursor.execute(query) # execute
 
+# print the select query
 chunks = cursor.fetchall()
 for chunk in chunks:
     print(f" - {chunk[0]}")
